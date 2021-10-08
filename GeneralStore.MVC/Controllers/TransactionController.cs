252245f5061d11059350f3d57b59f3dd8a480e79
kyle Joest <kjoest92@gmail.com>
2021-10-08 13:19:41 -0500

    Add project files.

---
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -0,0 +1,110 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View(_db.Transactions.ToList());
+        }
+
+        // GET: Transaction/{id}
+        public ActionResult Details(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            if(transaction == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(transaction);
+        }
+
+        // GET: Transaction/Create
+        // ViewData / ViewBags
+        public ActionResult Create()
+        {
+            var viewModel = new CreateTransactionViewModel();
+            viewModel.Customers = _db.Customers.Select(customer => new SelectListItem
+            {
+                Text = customer.FirstName + " " + customer.LastName,
+                Value = customer.CustomerId.ToString()
+            });
+            viewModel.Products = _db.Products.Select(product => new SelectListItem
+            {
+                Text = product.Name,
+                Value = product.ProductId.ToString()
+            });
+
+            return View(viewModel);
+        }
+
+        // POST: Transaction/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateTransactionViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+
+        // GET: Transaction/Delete/{id}
+        public ActionResult Delete(int id) 
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(transaction);
+        }
+
+        // POST: Transaction/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            Transaction transaction = _db.Transactions.Find(id); 
+            _db.Transactions.Remove(transaction);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Transaction/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            Transaction transation = _db.Transactions.Find(id);
+            if (transation == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewData["Customers"] = _db.Customers.Select(customer => new SelectListItem
+            {
+                Text = customer.FirstName + " " + customer.LastName,
+                Value = customer.CustomerId.ToString()
+            });
+            ViewData["Products"] = _db.Products.Select(product => new SelectListItem
+            {
+                Text = product.Name,
+                Value = product.ProductId.ToString()
+            });
+
+            return View(transation);
+        }
+
+        // POST: Transaction/Edit/{id}
+        [HttpPost]
+        public ActionResult Edit(Transaction transaction)
+        {
+            return View(transaction);
+        }
+    }
+}
